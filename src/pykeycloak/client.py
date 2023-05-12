f1b35f5392f9de3786f84566bd2bbbada05fc486
DarkKlaw <william.larocque@cse-cst.gc.ca>
2023-05-12 15:53:34 +0000

    Moved tokens to file. Added file lock to allow multi-process access to tokens

--- src/pykeycloak/client.py
+++ src/pykeycloak/client.py
@@ -1,18 +1,57 @@
+import os
+from typing import Optional, Union, Any
+from pydantic import BaseModel, SecretStr, FilePath, HttpUrl, validate_arguments, parse_obj_as
+from filelock import FileLock
 from keycloak.realm import KeycloakRealm
+from keycloak.openid_connect import KeycloakOpenidConnect
+from datetime import datetime
+import json
 import time
-from math import floor
 import warnings
+from pathlib import Path
+
+class ClientConfig(BaseModel):
+    server_url: HttpUrl
+    realm_name: str
+    client_id: str
+    client_secret: SecretStr
+    token_filename: Optional[FilePath] = None
+    access_token: Optional[str] = None
+    refresh_token: Optional[str] = None
+    verify: Union[bool, str] = True
+
+class TokenFileContent(BaseModel):
+    server_url: HttpUrl
+    realm_name: str
+    token_timestamp: datetime
+    access_token: str
+    access_token_lifespan: int = -1
+    refresh_token: Union[str, None]
+    refresh_token_lifespan: int = -1
+
+    def to_json(self) -> dict:
+        return {
+            'server_url': self.server_url,
+            'realm': self.realm_name,
+            'token_timestamp': self.token_timestamp.isoformat(),
+            'access_token': self.access_token,
+            'access_token_lifespan': self.access_token_lifespan,
+            'refresh_token': self.refresh_token,
+            'refresh_token_lifespan': self.refresh_token_lifespan
+        }
 
 class Client(object):
-    _realm = None
-    _client = None
-    _token_timestamp = None
-    _access_token = None
-    _access_token_lifespan = -1  # Value to specify we do not know expiry
-    _refresh_token = None
-    _refresh_token_lifespan = -1  # Value to specify we do not know expiry
+    _realm: KeycloakRealm
+    _client: KeycloakOpenidConnect
+    __default_token_filename = './.pykeycloak/{}.tok'
+    __token_filename: FilePath
+    __lock_filename: FilePath
 
-    def __init__(self, config, username: str = None, password: str = None):
+    @validate_arguments
+    def __init__(
+        self, 
+        config: ClientConfig
+    ):
         '''
             config: A map with the following keys
               'server_url': keycloak base server URL
@@ -21,115 +60,192 @@ class Client(object):
               'client_secret': secret needed to connect to Keycloak as the client
               'access_token': initial access_token (optional)
               'refresh_token': initial refresh_token (optional)
-              'verify': either 'true|false' or the path to the ca cert
+              'verify': either 'true|false' or the path to the ca cert. Defaults to True
             username: username of the user we want to get the token for (if config['access_token'] and config['refresh_token'] are not given)
             password: password of the user we want to get the token for (if config['access_token'] and config['refresh_token'] are not given)
         '''
         self.config = config
         # Connect to Keycloak
-        self._realm = KeycloakRealm(self.config['server_url'], self.config['realm_name'])
+        self._realm: KeycloakRealm = KeycloakRealm(self.config['server_url'], self.config['realm_name'])
         self._realm.client.session.verify = self.config['verify']
-        self._client = self._realm.open_id_connect(self.config['client_id'],
-                                        self.config['client_secret'])
-        # Initialize the tokens
-        try:
-            self._access_token = self.config['access_token']
-            self._refresh_token = self.config['refresh_token']
-            # Eagerly refresh the tokens so we know the expiry
-            self.refresh_tokens()
-        except KeyError:
-            if username and password:
-                self.password_credentials(username, password)
-            else:
-                raise ValueError('Initial Tokens in config dict or username and password arguments must be provided.')
+        self._client: KeycloakOpenidConnect = self._realm.open_id_connect(
+            self.config['client_id'],
+            self.config['client_secret']
+        )
+        # Prep the files
+        if config.token_filename is not None:
+            self.__token_filename = self.__token_filename.resolve()
+            if not config.token_filename.parent.exists():
+                os.makedirs(str(config.token_filename.parent), exist_ok=True)
+            self.__token_filename = config.token_filename
+        else:
+            os.makedirs('./.pykeycloak', exist_ok=True)
+            self.__token_filename = FilePath(self.__default_token_filename.format(self.config['realm_name']))
+            self.__token_filename = self.__token_filename.resolve()
+        self.__lock_filename = self.__token_filename.with_suffix('.lock')
+        self.__lock = FileLock(self.__lock_filename)
 
-    def get_token_timestamp(self):
+    @validate_arguments
+    async def initialize_tokens(
+        self,
+        username: Optional[str] = None, 
+        password: Optional[SecretStr] = None
+    ) -> TokenFileContent:
+        # Initialize the tokens
+        with self.__lock:
+            try:
+                if self.__token_filename.exists():
+                    with open(self.__token_filename, 'r') as token_file:
+                        token_file_contents = parse_obj_as(TokenFileContent, json.load(token_file))
+                    # Check if the token is still valid
+                    now = datetime.now()
+                    if token_file_contents.access_token_lifespan < 0:
+                        warnings.warn('We do not know if the access token has expired or not.')
+                    elif now > token_file_contents.token_timestamp + token_file_contents.access_token_lifespan:
+                        token_file_contents = await self.refresh_tokens(
+                            token_file_contents = token_file_contents
+                        ) # Refresh the token since it has expired
+                else:
+                    # Eagerly refresh the tokens so we know the expiry
+                    token_file_contents = await self.refresh_tokens(
+                        token_file_contents = TokenFileContent(
+                            server_url=self.config.server_url,
+                            realm_name=self.config.realm_name,
+                            token_timestamp=datetime.now(),
+                            access_token=self.config['access_token'],
+                            refresh_token=self.config['refresh_token']
+                        )
+                    )
+            except KeyError:
+                if username and password:
+                    token_file_contents = await self.password_credentials(username, password)
+                else:
+                    raise ValueError('Initial Tokens in config dict or username and password arguments must be provided.')
+        return token_file_contents
+            
+    def __parse_response(self, response: dict) -> TokenFileContent:
+        with self.__lock:
+            if not ('access_token' in response):
+                raise KeyError('Response does not contain an access token')
+            token_timestamp = datetime.now()
+            access_token = response['access_token']
+            if 'refresh_token' in response:
+                refresh_token = response['refresh_token']
+            else:
+                refresh_token = None
+            if 'expires_in' in response:
+                access_token_lifespan = int(response['expires_in'])
+            else:
+                access_token_lifespan = -1 # Value to specify we do not know expiry
+            if 'refresh_expires_in' in response:
+                refresh_token_lifespan = int(response['refresh_expires_in'])
+            else:
+                refresh_token_lifespan = -1 # Value to specify we do not know expiry
+            token_file_contents = TokenFileContent(
+                token_timestamp=token_timestamp,
+                access_token=access_token,
+                refresh_token=refresh_token,
+                access_token_lifespan=access_token_lifespan,
+                refresh_token_lifespan=refresh_token_lifespan
+            )
+            with open(self.__token_filename, 'w') as token_file:
+                json.dump(token_file_contents.to_json(), token_file)
+            return token_file_contents
+    
+    async def __get_token_attributes(self) -> TokenFileContent:
+        if self.__token_filename.exists():
+            with self.__lock:
+                with open(self.__token_filename, 'r') as token_file:
+                    token_file_contents = parse_obj_as(TokenFileContent, json.load(token_file))
+                return token_file_contents
+        else:
+            raise FileNotFoundError(f'No existing token found at {self.__token_filename}!')
+
+    async def get_token_timestamp(self) -> datetime:
         '''
             Return the timestamp when the tokens where received from the auth server.
             None if unknown
         '''
-        return self._token_timestamp
+        token_file_contents = await self.__get_token_attributes()
+        return token_file_contents.token_timestamp
 
-    def get_access_token_expiry_timestamp(self):
+    async def get_access_token_expiry_timestamp(self) -> Union[datetime, None]:
         '''
             Return the timestamp when the access token expires
             None if unknown
         '''
-        if self._access_token_lifespan < 0:
+        token_file_contents = await self.__get_token_attributes()
+        if token_file_contents.access_token_lifespan < 0:
             return None
-        return self._token_timestamp + self._access_token_lifespan
+        return token_file_contents.token_timestamp + token_file_contents.access_token_lifespan
 
-    def get_refresh_token_expiry_timestamp(self):
+    async def get_refresh_token_expiry_timestamp(self) -> Union[datetime, None]:
         '''
             Return the timestamp when the refresh token expires
             None if unknown
         '''
-        if self._refresh_token_lifespan < 0:
+        token_file_contents = await self.__get_token_attributes()
+        if token_file_contents.refresh_token_lifespan < 0:
             return None
-        return self._token_timestamp + self._refresh_token_lifespan
+        return token_file_contents.token_timestamp + token_file_contents.refresh_token_lifespan
     
-    def get_access_token(self):
-        if self._access_token_lifespan >= 0 and time.time() > (self._token_timestamp + self._access_token_lifespan):
-            self.refresh_tokens() # Refresh the token since it has expired
-        elif self._access_token_lifespan < 0:
-            warnings.warn('We do not know if the access token has expired or not.')
-        return self._access_token
+    async def get_access_token(self) -> str:
+        with self.__lock:
+            token_file_contents = await self.__get_token_attributes()
+            now = datetime.now()
+            access_token_expiry = token_file_contents.token_timestamp
+            if token_file_contents.access_token_lifespan < 0:
+                warnings.warn('We do not know if the access token has expired or not.')
+            elif now > access_token_expiry:
+                new_token_file_contents = await self.refresh_tokens(
+                    token_file_contents = token_file_contents
+                ) # Refresh the token since it has expired
+                return new_token_file_contents.access_token
+            return token_file_contents.access_token
 
-    def get_refresh_token(self):
-        if self._refresh_token is None:
-            raise ValueError('Do not have a refresh token available.')
-        elif self._refresh_token_lifespan >= 0 and time.time() > (self._token_timestamp + self._refresh_token_lifespan):
-            # Refresh token has expired
-            warnings.warn('Refresh token has expired. Use password_credentials(username: str, password: str) to get new tokens')
-        elif self._refresh_token_lifespan < 0:
-            warnings.warn('We do not know if the refresh token has expired or not.')
-        return self._refresh_token
+    async def get_refresh_token(self) -> Union[str, None]:
+        with self.__lock:
+            token_file_contents = await self.__get_token_attributes()
+            if token_file_contents.refresh_token is None:
+                return None
+            now = datetime.now()
+            refresh_token_expiry = token_file_contents.token_timestamp + token_file_contents.refresh_token_lifespan
+            if token_file_contents.refresh_token_lifespan >= 0 and now > refresh_token_expiry:
+                # Refresh token has expired
+                warnings.warn('Refresh token has expired. Use password_credentials(username: str, password: str) to get new tokens')
+            elif token_file_contents.refresh_token_lifespan < 0:
+                warnings.warn('We do not know if the refresh token has expired or not.')
+            return token_file_contents.refresh_token
 
-    def get_user_info(self):
-        return self._client.userinfo(self._access_token)
+    async def get_user_info(self) -> Any:
+        with self.__lock:
+            return self._client.userinfo(await self.get_access_token())
 
-    def parse_response(self, response: dict):
-        if not ('access_token' in response):
-            raise KeyError('Response does not contain an access token')
-        self._token_timestamp = floor(time.time()) # Current time in seconds
-        self._access_token = response['access_token']
-        if 'refresh_token' in response:
-            self._refresh_token = response['refresh_token']
-        else:
-            self._refresh_token = None
-        if 'expires_in' in response:
-            self._access_token_lifespan = int(response['expires_in'])
-        else:
-            self._access_token_lifespan = -1 # Value to specify we do not know expiry
-        if 'refresh_expires_in' in response:
-            self._refresh_token_lifespan = int(response['refresh_expires_in'])
-        else:
-            self._refresh_token_lifespan = -1 # Value to specify we do not know expiry
-
-    def refresh_tokens(self):
+    async def refresh_tokens(self, token_file_contents: TokenFileContent) -> TokenFileContent:
         '''
             Attempt to refresh the tokens using the stored refresh token
         '''
-        if self._refresh_token is None:
-            raise ValueError('Do not have a refresh token available. Use password_credentials(username: str, password: str) instead.')
-        elif self._refresh_token_lifespan >= 0 and time.time() > (self._token_timestamp + self._refresh_token_lifespan):
-            raise RuntimeError('Refresh token has expired. Use password_credentials(username: str, password: str) to refresh tokens.')
-        elif self._refresh_token_lifespan < 0:
-            warnings.warn('Will try to refresh tokens using internal refresh token. Do not know if token has expired or not.')
-        res = self._client.refresh_token(self._refresh_token)
-        self.parse_response(res)
+        with self.__lock:
+            if token_file_contents.refresh_token is None:
+                raise ValueError('Do not have a refresh token available. Use password_credentials(username: str, password: str) instead.')
+            elif token_file_contents.refresh_token_lifespan >= 0 and time.time() > (self._token_timestamp + self._refresh_token_lifespan):
+                raise RuntimeError('Refresh token has expired. Use password_credentials(username: str, password: str) to refresh tokens.')
+            elif token_file_contents.refresh_token_lifespan < 0:
+                warnings.warn('Will try to refresh tokens using internal refresh token. Do not know if token has expired or not.')
+            res = self._client.refresh_token(token_file_contents.refresh_token)
+            return self.__parse_response(res)
 
-    def password_credentials(self, username: str, password: str):
+    async def password_credentials(self, username: str, password: SecretStr) -> TokenFileContent:
         '''
             create new tokens using username and password
         '''
         res = self._client.password_credentials(username, password)
-        self.parse_response(res)
+        return self.__parse_response(res)
 
-    def token_exchange(self, audience):
+    async def token_exchange(self, audience) -> Any:
         '''
             return a new token for audience (local client within the same realm) based on 
             the current access_token
         '''
-        res = self._client.token_exchange(subject_token = self.get_access_token(), audience = audience)
+        res = self._client.token_exchange(subject_token = await self.get_access_token(), audience = audience)
         return res
